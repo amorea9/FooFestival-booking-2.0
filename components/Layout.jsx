48c14168bcf09c8c77df93ef8709be5c3b57fd2a
Alessia <98785381+amorea9@users.noreply.github.com>
2023-01-16 15:06:17 +0100

    removed favicon fix

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -12,7 +12,7 @@ function Layout({ children }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="FOOFEST | The best damned festival - book your tickets now " />
         <meta name="keywords" content="HTML, SASS, JavaScript, React.js, Next.js Exam project" />
-        <link rel="shortcut icon" type="image/jpg" src={favicon} />
+        {/* <link rel="shortcut icon" type="image/jpg" src={favicon} /> */}
       </Head>
       <Nav />
       <main>{children}</main>
